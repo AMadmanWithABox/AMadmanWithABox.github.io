8453de631b449484e40724f6a91ec7652dfe4fe2
AMadmanWithABox <carter.gamble014@gmail.com>
2022-01-30 17:52:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const weather1 = document.getElementById('weather1');
 const temp1 = document.getElementById('temp1');
 const weatherIcon = document.getElementById('weatherIcon');
 const highLow = document.getElementById("highLow");
+const timeBox = document.getElementById("timeBox");
+const weatherIconSmall = document.getElementById("weatherIconSmall");
+const tempHourly = document.getElementById("tempHourly");
 
 var lat, lon;
 
@@ -26,14 +29,17 @@ fetch(locationUrl)
       .then(response => response.json())
       .then(data => {
         console.log(data);
+        var time = new Date(data.current.dt * 1000);
         var temp = Math.round(data.current.temp);
         var icon = data.current.weather[0].icon;
         weather0.innerHTML = data.current.weather[0].main;
         temp0.innerHTML = temp + '&deg;';
         weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@4x.png">`;
         highLow.innerHTML = "&uarr; " + Math.round(data.daily[0].temp.max) + '&deg; &darr; ' + Math.round(data.daily[0].temp.min) + '&deg;';
-        weather1.innerHTML = data.daily[1].weather[0].main;
-        temp1.innerHTML = data.daily[1].temp.max + ' &deg;F';
+        data.hourly.forEach(element => {
+          console.log("Here!");
+          timeBox.innerHTML +='<span class="hfcTime"><div>'+ new Date(element.dt * 1000).toLocaleString("en-US", {hour: "numeric"}) + `</div><div><img src="http://openweathermap.org/img/wn/${element.weather[0].icon}.png"></div><div>${Math.round(element.temp)}&deg;</div>` +'</span>';
+        });
       })
   .catch(e => console.log(e));
   })
